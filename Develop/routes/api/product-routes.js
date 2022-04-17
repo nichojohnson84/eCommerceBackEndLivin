23333338a2b012a4623c4f9fcf1b694a80a713a1
Nick Johnson <nickcjohnson84@outlook.com>
2022-04-17 17:43:12 -0600

    Product Routes almost done NewPro needs work

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -64,13 +64,13 @@ router.get('/:id', (req, res) => {
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
+  /*req.body should look like this ...
+  {
+    product_name: "Ham Sandwich",
+    price: 200.00,
+    stock: 3,
+    tagIds: [1, 2, 3, 4]
+  }
   */
   Product.create(req.body)
     .then((product) => {
@@ -82,6 +82,7 @@ router.post('/', (req, res) => {
             tag_id,
           };
         });
+
         return ProductTag.bulkCreate(productTagIdArr);
       }
       // if no product tags, just respond
@@ -131,13 +132,29 @@ router.put('/:id', (req, res) => {
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
+      console.log(err);
       res.status(400).json(err);
     });
 });
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbProductData) => {
+      if (!dbProductData) {
+        res.status(404).json({ message: 'No Product with this ID' });
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
